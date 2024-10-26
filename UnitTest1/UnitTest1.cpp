cf138f8e5f83251408910a9f1b03cbbe9440e9d3
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-26 11:38:48 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab8.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestProcessCharacter)
+		{
+			Assert::AreEqual('z', ProcessCharacter('z'));
+			Assert::AreEqual('a', ProcessCharacter('A'));
+			Assert::AreEqual('1', ProcessCharacter('1'));
+		}
+		TEST_METHOD(TestIsAlphanumeric)
+		{
+			Assert::IsTrue(IsAlphanumeric('A')); 
+			Assert::IsTrue(IsAlphanumeric('z')); 
+			Assert::IsTrue(IsAlphanumeric('5')); 
+			Assert::IsFalse(IsAlphanumeric('!'));
+		}
+		TEST_METHOD(TestProcessString)
+		{
+			Assert::AreEqual(string("a1b2c3"), ProcessString("A1B2!C3 D4"));
+			Assert::AreEqual(string("abc"), ProcessString("ABC"));
+			Assert::AreEqual(string("abc123"), ProcessString("abc123 !@#"));
+		}
+	};
+}
---
+++ lab8.2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Функція для обробки символа: конвертує великі літери в малі та повертає їх
+char ProcessCharacter(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c + ('a' - 'A'); // Перетворюємо великі літери на малі
+    }
+    return c; // Повертаємо малу літеру або цифру без змін
+}
+
+// Функція для перевірки, чи символ є літерою або цифрою
+bool IsAlphanumeric(char c) {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
+}
+
+// Основна функція обробки рядка
+string ProcessString(const string& s) {
+    string result;
+    for (size_t i = 0; i < s.length(); ++i) {
+        if (s[i] == ' ') {
+            break; // Зупиняємося на першому пробілі
+        }
+        // Додаємо тільки алфавітні символи та цифри після обробки
+        if (IsAlphanumeric(s[i])) {
+            result += ProcessCharacter(s[i]); // Додаємо оброблений символ до результату
+        }
+    }
+    return result;
+}
+
+int main() {
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+    cout << "Processed string: " << ProcessString(str) << endl;
+    return 0;
+}
